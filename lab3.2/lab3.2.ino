79582d1b16396f8a2aba15eaf2072da8da8f7dc1
Cody Constine <cconstine@knowledgefactor.com>
2016-10-06 18:10:34 -0600

    Adding 3.2

--- lab3.2/lab3.2.ino
+++ lab3.2/lab3.2.ino
@@ -9,7 +9,105 @@ double theta;
 bool start;
 short servo;
 short grid[4][4];
+short xPos;
+short yPos;
+struct position
+{
+  short x;
+  short y;
+};
+short path[16];
 
+
+typedef struct position Position;
+Position index[16];
+
+int pointToIndex(int x, int y)
+{
+  x = x*10;
+  y = y*10;
+  return grid[x][y];
+}
+void indexAssign(){
+  int i,ii;
+  int counter = 0;
+   for(i=0;i<5;i++)
+    {
+      for(ii=0;ii<5;ii++)
+      {
+        index[counter].x = i;
+        index[counter].y = ii;
+        counter++; 
+      }
+    }
+}
+int getCords(int i)
+{
+  return index[i].x, index[i].y;
+}
+
+int getWeight(int s, int e)
+{
+  int xS, yS, xE, yE;
+  xS, yS = getCords(s);
+  xE, yE = getCords(e);
+  if(grid[xS][yS] == 0 || grid[xE][yE] == 0)
+  {
+    return 99;
+  }
+  else if(abs(xS-xE) == 1 && abs(yS-yE) == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return 99;
+  }
+}
+
+
+void dij(int n,int v,int cost[10][10],int dist[])
+{
+ int i,u,count,w,flag[10],min;
+ for(i=1;i<=n;i++)
+ {
+  flag[i]=0,dist[i]=getWeight(v,i);
+ }
+ count=2;
+ while(count<=n)
+ {
+  min=99;
+  for(w=1;w<=n;w++)
+  {
+   if(dist[w]<min && !flag[w])
+   {
+    min=dist[w],u=w;
+   }
+  }
+  flag[u]=1;
+  count++;
+  for(w=1;w<=n;w++)
+  {
+   if((dist[u]+getWeight(u,w)<dist[w]) && !flag[w]) 
+   {
+    dist[w]=dist[u]+getWeight(u,w);
+    path[w]=u;
+   }
+  }
+ }
+}
+
+int * pathReturn(int start, int source){
+  int next = start;
+  int r[16];
+  int i = 0;
+  while(next != source){
+    r[i] = next;
+    next = path[next];
+    i++;
+  }
+  return r;
+}
 
 void setup(){
   //Serial.begin(9600);
@@ -31,7 +129,13 @@ void setup(){
         grid[i][ii]=1;
       }
     }
+    sparki.servo(-45);
+    xPos = 0;
+    yPos = 0;
 }
+
+
+
 
 void loop(){
   double xdot = 0;
@@ -60,16 +164,16 @@ void loop(){
 
   if(lineCenter < threshold && lineLeft < threshold && lineRight < threshold && start)
   {
-    xi=0;
-    yi=0;
+    xi=30;
+    yi=5;
     theta=0.0;
     start = false;
     sparki.beep(); 
   }
   else if(lineCenter < threshold && lineLeft < threshold && lineRight < threshold && !start && (xi <= .1 && xi >= -.1)&&(yi <= .1 && yi >= -.1))
   {
-    xi=0;
-    yi=0;
+    xi=30;
+    yi=5;
     theta=0.0;
     start = false; 
     sparki.beep();
@@ -90,29 +194,15 @@ void loop(){
     sparki.moveRight(); // turn right
     xdot = 0; thetadot = -speedTurning;
   }
-  
-  int place = (time/100)%10;
-  int servoTheta = 0;
-  if(place < 2){
-    sparki.servo(-45);
-    servoTheta = -M_PI/4;
-  }
-  else if(place < 5){
-    sparki.servo(0);
-    servoTheta = 0;
-  }
-  else if(place < 8){
-    sparki.servo(45);
-    servoTheta = M_PI/4;
-  }
-  else if(place < 10){
-    sparki.servo(0);
-    servoTheta = 0;
-  }
   int ping = sparki.ping();
   if(ping < 15){
-    int x = ping*cos(servoTheta);
-    int y = ping*sin(servoTheta);
+    int x = ping*cos(M_PI/4);
+    int y = ping*sin(M_PI/4);
+    int xPos = xi+x*cos(theta);
+    int yPos = yi+y*sin(theta);
+    x = xPos*10;
+    y = yPos*10;
+    grid[x][y] = 0;
   }
   // Integration
 
